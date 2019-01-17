60099f90981506c7dd4ca608b88a058faae0a352
Rob <bruno.robsiq@gmail.com>
2019-01-17 15:06:48 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,107 @@
+const WebSocket = require('ws');
+
+class Server {
+  constructor(port) {
+    this.wss = new WebSocket.Server({ port });
+    this.users = {};
+    this.wss.on('connection', (ws) => {
+      ws.on('message', (message) => {
+        const msg = JSON.parse(message);
+        if (!msg.type) {
+          console.log('Invalid message');
+          console.log(msg)
+          return;
+        }
+        switch (msg.type) {
+          case 'signin':
+            this.handleUserConnection(msg, ws);
+            this.broadcastUsers();
+            break;
+          case 'offer':
+            this.handleRequestConnection(msg);
+            break;
+          case 'answer':
+            this.handleAnswer(msg);
+            break;
+          case 'newCandidate':
+            this.handleNewIceCandidate(msg);
+            break;
+          case 'requestUsers':
+            this.handleUsersRequest(msg);
+            break;
+          default:
+            console.log(`Message of type: ${msg.type} not handled.`);
+            break;
+        }
+      });
+      ws.on('close', (connection) => {
+        const user = Object.keys(this.users).find(k => this.users[k] === ws);
+        delete this.users[user];
+        console.log(`${user} has disconnected.`);
+        this.broadcastUsers();
+      });
+    });
+  }
+
+  handleUsersRequest(msg) {
+    const requester = msg.name;
+    const users = Object.keys(this.users);
+    console.log(`${requester} has requested online users`)
+    this.users[requester].send(JSON.stringify({
+      type: 'userList',
+      users: users,
+    }));
+  }
+
+  broadcastUsers() {
+    const users = Object.keys(this.users);
+    console.log(`Broadcasting userList to ${users.length} users.`)
+    this.wss.clients.forEach((client) => {
+      console.log(`Sending users to ${Object.keys(this.users).find(u => this.users[u] === client)}`)
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({
+          type: 'userList',
+          users: users,
+        }));
+      }
+    });
+  }
+
+  handleNewIceCandidate(msg) {
+    console.log(`${msg.name} has sent candidate to ${msg.dest}`);
+    if (msg.dest in this.users) {
+      this.users[msg.dest].send(JSON.stringify(msg));
+    }
+  }
+
+  handleUserConnection(msg, ws) {
+    console.log(`${msg.name} has connected`);
+    this.users[msg.name] = ws;
+  }
+
+  handleRequestConnection(msg) {
+    console.log(`${msg.name} has called ${msg.dest}`);
+    if (msg.dest in this.users) {
+      this.users[msg.dest].send(JSON.stringify({
+        type: msg.type,
+        sdp: msg.sdp,
+        name: msg.name,
+      }));
+    }
+  }
+
+  handleAnswer(msg) {
+    console.log(`${msg.dest} has been answered`);
+    if (msg.dest in this.users) {
+      this.users[msg.dest].send(JSON.stringify({
+        type: msg.type,
+        sdp: msg.sdp,
+        name: msg.name,
+      }));
+    }
+  }
+}
+
+new Server(8090);
+
+module.exports.Server = Server;
